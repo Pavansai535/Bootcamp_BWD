58224aec495a5dbec11271d37c535ab958ecf1c4
Pavansai535 <71284789+Pavansai535@users.noreply.github.com>
2021-06-06 23:24:23 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>SuperCell</h1>
+      <p>
+        Clash Royale is a freemium real-time strategy video game developed and
+        published by Supercell. The game combines elements from collectible card
+        games, tower defense, and multiplayer online battle arena.{" "}
+      </p>
+    </div>
+  );
+}
+export default Note;
